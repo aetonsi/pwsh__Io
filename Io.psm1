163731638adb9c413de82196552c7573d7c6fab7
aetonsi <18366087+aetonsi@users.noreply.github.com>
2023-02-27 16:08:41 +0100

    add functions: [Get-Dirname, Get-Basename, Get-Realpath, Get-TypedPath, Test-IsDir, Test-IsFile, New-TemporaryDirectory ( transferred from aetonsi/pwsh__Utils)], dialog functions now ask to type path if OS = linux

--- Io.psm1
+++ Io.psm1
@@ -1,7 +1,140 @@
-Add-Type -AssemblyName System.Windows.Forms
+# save/open dialogs Filter example: 'Documents (ms office only)|*.docx|SpreadSheets (ms office, openoffice)|*.xlsx,*.xls,*.ods'
 
-# Filter example 'Documents (ms office only)|*.docx|SpreadSheets (ms office, openoffice)|*.xlsx,*.xls,*.ods'
-# TODO suppress ALL dialogs output
+using namespace Microsoft.PowerShell.Commands
+
+Import-Module "$PSScriptRoot/pwsh__String/String.psm1"
+
+
+function path_cleanup([parameter(ValueFromPipeline)][string] $path) {
+  $result = $path | Get-UnquotedString # remove quotes if present
+  if ($result -eq '.') { $result = $PWD.Path } # resolve the dot
+  if ($result -eq '..') { $result = "$($PWD.Path)/.." } # resolve the double dot
+  if ($result -like '*:') { $result = "$path/" } # normalizes drives roots
+  $result = [System.IO.Path]::TrimEndingDirectorySeparator($result) # strips the final slash if present
+  return $result
+}
+
+
+function Get-Dirname ([parameter(ValueFromPipeline)][string] $path) {
+  $result = $path | path_cleanup
+  $result = [System.IO.Path]::GetDirectoryName($result)
+  if (! $result) { $result = [System.IO.Path]::GetFullPath($path) } # for drive roots getdirectoryname() would return null
+  $result = [System.IO.Path]::GetFullPath("$result/..")
+  return ($result | path_cleanup)
+}
+function Get-Basename ([parameter(ValueFromPipeline)][string] $path, [switch] $stripExtension) {
+  $result = $path | path_cleanup
+  $result = [System.IO.Path]::GetFullPath($path)
+  if ($stripExtension) {
+    $result = [System.IO.Path]::GetFileNameWithoutExtension($result)
+  }
+  else {
+    $result = [System.IO.Path]::GetFileName($result)
+  }
+  return $result
+}
+function Get-Realpath ([parameter(ValueFromPipeline)][string] $path) {
+  $result = $path | path_cleanup
+  $result = [System.IO.Path]::GetFullPath($result)
+  return ($result | path_cleanup)
+}
+
+
+function Get-TypedPath (
+  [TestPathType] $pathType = ([TestPathType]::Any),
+  [switch] $mustExist = $false,
+  [switch] $allowMultiple = $false,
+  [bool] $returnAbsolutePath = $true
+) {
+  $pathType = [TestPathType]::Leaf
+  switch ($pathType) {
+    ([TestPathType]::Any) { $typeStr = 'file or folder' ; break }
+    ([TestPathType]::Container) { $typeStr = 'folder' ; break }
+    ([TestPathType]::Leaf) { $typeStr = 'file' ; break }
+    default { throw "unknown path type: $pathType" }
+  }
+  $promptStr = "$(
+    if($mustExist){
+      if($allowMultiple){'one or more existing'}else{'an existing'}
+    }else{
+      if($allowMultiple){'one or more'}else{'a'}
+    }) $typeStr"
+
+
+  if ($allowMultiple) {
+    $promptStr += ' [you can enter multiple paths separating them with a space; if a path contains a space, you MUST enclose it with double quotes]'
+  }
+
+  $invalidPaths = $null
+  do {
+    if ($null -ne $invalidPaths) {
+      Write-Warning "The following path(s) you entered do(es) not exist or is not of the correct type ($typeStr):`n$invalidPaths"
+    }
+    $paths = Read-Host -Prompt "Please type a path to ${promptStr}"
+    if (! $paths) { $invalidPaths = @('') ; continue }
+    if ($allowMultiple) {
+      $paths = Get-TokenizedCommandLine $paths
+    }
+    else {
+      $paths = [string[]] @($paths)
+    }
+    $invalidPaths = @()
+    for ($i = 0; $i -lt $paths.Length ; $i++) {
+      if ($mustExist) {
+        # checks both for existance AND type (folder/file)
+        if (! (Test-Path -PathType $pathType -Path ($paths[$i] | path_cleanup))) {
+          $invalidPaths += $paths[$i]
+        }
+      }
+      else {
+        # manually check if dir or file (can only distinguish them by the trailing slash)
+        if (($pathType -eq [TestPathType]::Leaf) -and ($paths[$i] -match '.*[/\\:]$')) {
+          $invalidPaths += $paths[$i]
+        }
+      }
+    }
+  } until($invalidPaths.Length -eq 0)
+
+  if ($returnAbsolutePath) {
+    for ($i = 0; $i -lt $paths.Length ; $i++) {
+      $paths[$i] = Get-Realpath $paths[$i]
+    }
+  }
+  if ($allowMultiple) {
+    return , $paths
+  }
+  else {
+    return $paths[0]
+  }
+}
+
+function Test-IsDir(
+  [Parameter(Position = 0, ValueFromPipeline = $true, Mandatory = $true)][string] $dir,
+  [Parameter(Mandatory = $false)][switch] $mustExist = $false
+) {
+  if ($mustExist -and (!(Test-Path $dir))) { throw "$dir does not exist" }
+  # return (Get-Item $dir) -is [System.IO.DirectoryInfo]
+  return Test-Path $dir -PathType Container
+}
+
+function Test-IsFile(
+  [Parameter(Position = 0, ValueFromPipeline = $true, Mandatory = $true)][string] $file,
+  [Parameter(Mandatory = $false)][switch] $mustExist = $false
+) {
+  if ($mustExist -and (!(Test-Path $file))) { throw "$file does not exist" }
+  # return (Get-Item $file) -is [System.IO.FileInfo]
+  return Test-Path $file -PathType Leaf
+}
+
+
+function New-TemporaryDirectory {
+  # https://stackoverflow.com/a/34559554
+  $parent = [System.IO.Path]::GetTempPath()
+  $name = [System.IO.Path]::GetRandomFileName()
+  return New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
+
 function Get-OpenFileDialog(
   [string] $InitialDirectory = $PWD,
   [bool] $MultiSelect = $false,
@@ -10,37 +143,52 @@ function Get-OpenFileDialog(
   [string] $defaultFilename = $null,
   [bool] $addAnyFileFilter = $true
 ) {
-  $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
-    InitialDirectory             = $InitialDirectory
-    MultiSelect                  = $MultiSelect
-    Filter                       = $Filter + ($addAnyFileFilter ? (($Filter ? '|' : '') + 'Any file|*') : '')
-    Title                        = $Title
-    FileName                     = $defaultFilename
-    CheckFileExists              = $true
-    CheckPathExists              = $true
-    ShowHiddenFiles              = $true
-    SupportMultiDottedExtensions = $true
-    ShowReadOnly                 = $false
-    AddExtension                 = $true
+  if ($IsWindows) {
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+      InitialDirectory             = $InitialDirectory
+      MultiSelect                  = $MultiSelect
+      Filter                       = $Filter + ($addAnyFileFilter ? (($Filter ? '|' : '') + 'Any file|*') : '')
+      Title                        = $Title
+      FileName                     = $defaultFilename
+      CheckFileExists              = $true
+      CheckPathExists              = $true
+      ShowHiddenFiles              = $true
+      SupportMultiDottedExtensions = $true
+      ShowReadOnly                 = $false
+      AddExtension                 = $true
+    }
+    $FileBrowser.ShowDialog() *>$null
+    return $MultiSelect ? ([string[]] $FileBrowser.FileNames) : $FileBrowser.FileName
   }
-  $FileBrowser.ShowDialog() *>$null
-  return $MultiSelect ? ([string[]] $FileBrowser.FileNames) : $FileBrowser.FileName
+  elseif ($IsLinux) {
+    $path = Get-TypedPath -pathType Leaf -mustExist -allowMultiple:$MultiSelect
+  }
+  return $path
 }
+
 function Get-FolderBrowserDialog(
   [string] $InitialDirectory = $PWD,
   [bool] $ShowNewFolderButton = $true,
   [string] $Title = $null
 ) {
-  $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{
-    InitialDirectory       = $InitialDirectory
-    ShowNewFolderButton    = $ShowNewFolderButton
-    Description            = $Title
-    UseDescriptionForTitle = $true
-    RootFolder             = 'Desktop'
-    ShowHiddenFiles        = $true
+  if ($IsWindows) {
+    Add-Type -AssemblyName System.Windows.Forms
+    $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{
+      InitialDirectory       = $InitialDirectory
+      ShowNewFolderButton    = $ShowNewFolderButton
+      Description            = $Title
+      UseDescriptionForTitle = $true
+      RootFolder             = 'Desktop'
+      ShowHiddenFiles        = $true
+    }
+    $FolderBrowser.ShowDialog() *>$null
+    $path = $FolderBrowser.SelectedPath
   }
-  $FolderBrowser.ShowDialog() *>$null
-  return $FolderBrowser.SelectedPath
+  elseif ($IsLinux) {
+    $path = Get-TypedPath -pathType Container -mustExist
+  }
+  return $path
 }
 
 function Get-SaveFileDialog(
@@ -53,22 +201,29 @@ function Get-SaveFileDialog(
   [bool] $CreatePrompt = $false,
   [bool] $addAnyFileFilter = $true
 ) {
-  $FileBrowser = New-Object System.Windows.Forms.SaveFileDialog -Property @{
-    InitialDirectory             = $InitialDirectory
-    CheckWriteAccess             = $CheckWriteAccess
-    OverwritePrompt              = $OverwritePrompt
-    CreatePrompt                 = $CreatePrompt
-    Filter                       = $Filter + ($addAnyFileFilter ? (($Filter ? '|' : '') + 'Any file|*') : '')
-    Title                        = $Title
-    FileName                     = $defaultFilename
-    CheckPathExists              = $true
-    ShowHiddenFiles              = $true
-    SupportMultiDottedExtensions = $true
-    AddExtension                 = $true
+  if ($IsWindows) {
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.SaveFileDialog -Property @{
+      InitialDirectory             = $InitialDirectory
+      CheckWriteAccess             = $CheckWriteAccess
+      OverwritePrompt              = $OverwritePrompt
+      CreatePrompt                 = $CreatePrompt
+      Filter                       = $Filter + ($addAnyFileFilter ? (($Filter ? '|' : '') + 'Any file|*') : '')
+      Title                        = $Title
+      FileName                     = $defaultFilename
+      CheckPathExists              = $true
+      ShowHiddenFiles              = $true
+      SupportMultiDottedExtensions = $true
+      AddExtension                 = $true
+    }
+    $FileBrowser.ShowDialog() *>$null
+    $path = $FileBrowser.FileName
   }
-  $FileBrowser.ShowDialog() *>$null
-  return $FileBrowser.FileName
+  elseif ($IsLinux) {
+    $path = Get-TypedPath -pathType Leaf
+  }
+  return $path
 }
 
 
-Export-ModuleMember -Function *
+Export-ModuleMember -Function *-*
---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO
+
+- implement dialogs for linux (mono?) instead of the typed path(s)
+- suppress all system.windows.forms dialogs garbage output (windows)
